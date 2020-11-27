de903f2a532e14ad40a5247dd2c50a0b0b6758b6
Daniel Chaves Pereira <newarts88@hotmail.com>
2020-11-26 21:49:29 -0300

    aula 19 e 26/11

--- lab-angular/src/app/app.module.ts
+++ lab-angular/src/app/app.module.ts
@@ -6,17 +6,30 @@ import { AppComponent } from './app.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { ContadorComponent } from './contador/contador.component';
 import { CardComponent } from './card/card.component';
+import { FormReativoComponent } from './form-reativo/form-reativo.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms'
+
+19:56
+
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms'
+
+Pressione Shift + Tab para acessar o histórico do bate-papo. 
 
 @NgModule({
   declarations: [
     AppComponent,
     RodapeComponent,
     ContadorComponent,
-    CardComponent
+    CardComponent,
+    FormReativoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
